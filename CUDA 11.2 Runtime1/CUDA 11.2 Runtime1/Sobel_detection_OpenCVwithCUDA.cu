b63195c3015cdec1ce0f87ab9273112e0aaca2e6
LingoZHANG <gintoki.chel@gmail.com>
2021-04-17 19:41:48 +0200

    add Sobel_OpenCV+CUDA

---
+++ CUDA 11.2 Runtime1/CUDA 11.2 Runtime1/Sobel_detection_OpenCVwithCUDA.cu
@@ -0,0 +1,110 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cuda.h>
+#include <device_functions.h>
+#include <opencv2\opencv.hpp>
+#include <iostream>
+
+#include <windows.h>   
+#include <vector>
+#include <cstring>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <opencv2\imgproc\types_c.h>
+
+using namespace std;
+using namespace cv;
+ 
+
+//Sobel operator edge detection kernel function
+__global__ void sobelInCuda(unsigned char *dataIn, unsigned char *dataOut, int imgHeight, int imgWidth)
+{
+	int xIndex = threadIdx.x + blockIdx.x * blockDim.x;
+	int yIndex = threadIdx.y + blockIdx.y * blockDim.y;
+	int index = yIndex * imgWidth + xIndex;
+	int Gx = 0;
+	int Gy = 0;
+
+	if (xIndex > 0 && xIndex < imgWidth - 1 && yIndex > 0 && yIndex < imgHeight - 1)
+	{
+		Gx = dataIn[(yIndex - 1) * imgWidth + xIndex + 1] + 2 * dataIn[yIndex * imgWidth + xIndex + 1] + dataIn[(yIndex + 1) * imgWidth + xIndex + 1]
+			- (dataIn[(yIndex - 1) * imgWidth + xIndex - 1] + 2 * dataIn[yIndex * imgWidth + xIndex - 1] + dataIn[(yIndex + 1) * imgWidth + xIndex - 1]);
+		Gy = dataIn[(yIndex - 1) * imgWidth + xIndex - 1] + 2 * dataIn[(yIndex - 1) * imgWidth + xIndex] + dataIn[(yIndex - 1) * imgWidth + xIndex + 1]
+			- (dataIn[(yIndex + 1) * imgWidth + xIndex - 1] + 2 * dataIn[(yIndex + 1) * imgWidth + xIndex] + dataIn[(yIndex + 1) * imgWidth + xIndex + 1]);
+		dataOut[index] = (abs(Gx) + abs(Gy)) / 2;
+	}
+}
+
+//Sobel operator edge detection CPU function
+void sobel(Mat srcImg, Mat dstImg, int imgHeight, int imgWidth)
+{
+	int Gx = 0;
+	int Gy = 0;
+	for (int i = 1; i < imgHeight - 1; i++)
+	{
+		uchar *dataUp = srcImg.ptr<uchar>(i - 1);
+		uchar *data = srcImg.ptr<uchar>(i);
+		uchar *dataDown = srcImg.ptr<uchar>(i + 1);
+		uchar *out = dstImg.ptr<uchar>(i);
+		for (int j = 1; j < imgWidth - 1; j++)
+		{
+			Gx = (dataUp[j + 1] + 2 * data[j + 1] + dataDown[j + 1]) - (dataUp[j - 1] + 2 * data[j - 1] + dataDown[j - 1]);
+			Gy = (dataUp[j - 1] + 2 * dataUp[j] + dataUp[j + 1]) - (dataDown[j - 1] + 2 * dataDown[j] + dataDown[j + 1]);
+			out[j] = (abs(Gx) + abs(Gy)) / 2;
+		}
+	}
+}
+
+int main()
+{
+	Mat grayImg = imread("E:\\1.jpg",0);
+
+	int imgHeight = grayImg.rows;
+	int imgWidth = grayImg.cols;
+
+	imshow("src", grayImg);
+	Mat gaussImg;
+	//Gaussian filtering
+	GaussianBlur(grayImg, gaussImg, Size(3, 3), 0, 0, BORDER_DEFAULT);
+
+	//Sobel operator CPU implementation
+	Mat dst(imgHeight, imgWidth, CV_8UC1, Scalar(0));
+	sobel(gaussImg, dst, imgHeight, imgWidth);
+	imshow("CPU", dst);
+
+	//image of CUDA 
+	Mat dstImg(imgHeight, imgWidth, CV_8UC1, Scalar(0));
+
+	//GPU memory
+	unsigned char *d_in;
+	unsigned char *d_out;
+
+	cudaMalloc((void**)&d_in, imgHeight * imgWidth * sizeof(unsigned char));
+	cudaMalloc((void**)&d_out, imgHeight * imgWidth * sizeof(unsigned char));
+
+	//Gaussian filtered image from the CPU to the GPU
+	cudaMemcpy(d_in, gaussImg.data, imgHeight * imgWidth * sizeof(unsigned char), cudaMemcpyHostToDevice);
+
+	dim3 threadsPerBlock(32, 32);
+	dim3 blocksPerGrid((imgWidth + threadsPerBlock.x - 1) / threadsPerBlock.x, (imgHeight + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+	//kernel
+	sobelInCuda << <blocksPerGrid, threadsPerBlock >> > (d_in, d_out, imgHeight, imgWidth);
+
+	//back to the GPU
+	cudaMemcpy(dstImg.data, d_out, imgHeight * imgWidth * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+
+	imshow("CUDA", dstImg);
+
+	//Free GPU Memory
+	cudaFree(d_in);
+	cudaFree(d_out);
+	waitKey(0);
+	return 0;
+}
+
+
+
